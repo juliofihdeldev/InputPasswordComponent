f917f51c57b6a30473c2328e38c966908cf69a7f
Juli0 <julio.fils1@gmail.com>
2020-11-05 16:51:31 -0500

    React testing completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState , useEffect} from 'react';
+
+import axios from 'axios';
+const URL = 'http://hn.algolia.com/api/v1/search';
+function getUser() {
+  return Promise.resolve({ id: '1', name: 'Julio FILS' });
+}
+
+function sum(number1, number2) {
+  return number1 + number2
+} 
 
 function App() {
+
+  const [search, setSearch] = React.useState('');
+  const [user, setUser] = React.useState(null);
+
+  const [stories, setStories] = React.useState([]);
+  const [error, setError] = React.useState(null);
+ 
+  React.useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+ 
+    loadUser();
+  }, []);
+ 
+
+  async function handleFetch(event) {
+    let result;
+ 
+    try {
+      result = await axios.get(`${URL}?query=React`);
+ 
+      setStories(result.data.hits);
+    } catch (error) {
+      setError(error);
+    }
+  }
+ 
+  
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+
         <h1> Testing react </h1>
+        <div>  {user ? <p>Signed in as {user.name}</p> : null} </div>
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
@@ -19,6 +66,24 @@ function App() {
         >
           Learn React
         </a>
+
+
+        <button type="button" onClick={handleFetch}>
+          Fetch Stories
+      </button>
+        
+        {error && <span>Something went wrong ...</span>}
+        
+        <ul>
+        {stories.map((story) => (
+          <li key={story.objectID}>
+            <a href={story.url}>{story.title}</a>
+          </li>
+        ))}
+      </ul>
+
+        <button onClick={() => sum(12, 13)}> Submit </button>
+
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,75 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+
+
+describe('App', () => {
+
+
+  // test('fetches stories from an API and displays them', async () => {
+    
+  //   const stories = [
+  //     { objectID: '1', title: 'Hello' },
+  //     { objectID: '2', title: 'React' },
+  //   ];
+ 
+  //   const promise = Promise.resolve({ data: { hits: stories } });
+ 
+  //   axios.get.mockImplementationOnce(() => promise);
+ 
+  //   render(<App />);
+ 
+  //   await userEvent.click(screen.getByRole('button'));
+ 
+  //   await act(() => promise);
+ 
+  //   expect(screen.getAllByRole('listitem')).toHaveLength(2);
+  // });
+  
+  // test('fetches stories from an API and fails', async () => {
+  //   axios.get.mockImplementationOnce(() =>
+  //     Promise.reject(new Error())
+  //   );
+ 
+  //   render(<App />);
+ 
+  //   await userEvent.click(screen.getByRole('button'));
+ 
+  //   const message = await screen.findByText(/Something went wrong/);
+ 
+  //   expect(message).toBeInTheDocument();
+  // });
+
+
+
+  test('Test sign in ', async () => {
+    render(<App />);
+ 
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+
+  });
+
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+
+
+  test('renders testing in h1 ', () => {
+    render(<App />);
+    const titleh1Element = screen.getByText(/Testing react/i);
+    expect(titleh1Element).toBeInTheDocument();
+  });
 });
+ 
 
 
-test('renders testing in h1 ', () => {
-  render(<App />);
-  const titleh1Element = screen.getByText(/Testing react/i);
-  expect(titleh1Element).toBeInTheDocument();
-});
+
